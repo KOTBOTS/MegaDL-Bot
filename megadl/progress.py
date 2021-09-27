523937fdb8a176d586b9a54df92183fae1933068
KOTBOTS <89208197+KOTBOTS@users.noreply.github.com>
2021-09-27 20:02:52 +0530

    Update progress.py

--- megadl/progress.py
+++ megadl/progress.py
@@ -46,7 +46,7 @@ async def progress_for_pyrogram(
         )
         try:
             await message.edit(
-                text="**{}** {} \n\n**@AsmSafone | @SafoTheBot**".format(
+                text="**{}** {} \n\n**@KOT_BOTS | @KOT_REPORS**".format(
                     ud_type,
                     tmp
                 ),
