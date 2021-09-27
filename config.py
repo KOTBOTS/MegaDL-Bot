41330e3fd67bc19e6e6ad3c53f9315da3812af83
KOTBOTS <89208197+KOTBOTS@users.noreply.github.com>
2021-09-27 20:11:23 +0530

    Update config.py

--- config.py
+++ config.py
@@ -25,11 +25,11 @@ class TEXT:
 
 📡 **Hosted On:** [Heroku](https://heroku.com)
 
-🧑‍💻 **Developer:** [Safone](https://t.me/ImSafone)
+🧑‍💻 **Developer:** [KOT BOTS](https://t.me/KOT_BOTS)
 
-👥 **Support Group:** [SafoTheBot](https://t.me/safothebot)
+👥 **Support Group:** [support group]https://t.me/KOT_REPORS)
 
-📢 **Updates Channel:** [Ｓ１ ＢＯＴＳ](https://t.me/AsmSafone)
+📢 **Updates Channel:** [KOT BOTS](https://t.me/KOT_BOTS)
 """
 
   HELP_USER = """
@@ -37,7 +37,7 @@ This is **{bot_name}**
 
 This Bot Can Download Files & Videos From Mega Links & Upload To Telegram. Just Send Any Mega.nz Link & See The Magic. You Can Also Add or Change Caption: Just Select An Uploaded File/ Video or Forward Me Any Telegram File & Then Write The Text You Want To Be Caption On The File As A Reply To That File & The Text You Wrote Will Be Attached As Caption 😁! 
 
-**Made With ❤️ By @AsmSafone! 👑**
+**Made With ❤️ By @KOT_BOTS! 👑**
 """
 
   START_TEXT = """
